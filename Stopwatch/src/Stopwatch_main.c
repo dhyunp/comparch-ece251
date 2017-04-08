2c4238bdee2ae9f2dd6167cb5deaeab5e094fca6
e-chris <ce.chris.e@gmail.com>
2017-04-08 00:02:17 -0400

    Hey

---
+++ Stopwatch/src/Stopwatch_main.c
@@ -0,0 +1,20 @@
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include <SI_EFM8BB3_Register_Enums.h>                  // SFR declarations
+#include "InitDevice.h"
+// $[Generated Includes]
+// [Generated Includes]$
+
+//-----------------------------------------------------------------------------
+// main() Routine
+// ----------------------------------------------------------------------------
+int main(void) {
+	// Call hardware initialization routine
+	enter_DefaultMode_from_RESET();
+
+	while (1) {
+// $[Generated Run-time code]
+// [Generated Run-time code]$
+	}
+}
